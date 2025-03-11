6d57f61293eb4a054495679274f9d9e720536daa
Abdullatif Jarkas <110686370+abdullatif-jarkas@users.noreply.github.com>
2025-03-11 17:01:33 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,7 +2,6 @@ const Joi = require("joi");
 const { default: mongoose } = require("mongoose");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
-const bcrypt = require("bcryptjs")
 
 const UserSchema = new mongoose.Schema(
   {
